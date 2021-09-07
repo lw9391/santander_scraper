0aed1d6d8ba7ec509d4d2add3b38ba860d0ceb09
lw9391 <luk.wychowaniec@gmail.com>
2021-09-07 22:19:29 +0200

    Remove session state field check
    Removed session state field from SantanderSession along with its check
    during methods calls. Errors caused by incorrect methods sequence
    will be caught by SantanderAccountsScraperTest.

--- src/main/java/scraper/session/SantanderSession.java
+++ src/main/java/scraper/session/SantanderSession.java
@@ -5,14 +5,8 @@ import scraper.AccountDetails;
 import java.util.List;
 import java.util.Map;
 
-import static scraper.session.SessionState.FULLY_LOGGED;
-import static scraper.session.SessionState.NIK_ACCEPTED;
-import static scraper.session.SessionState.NONE;
-import static scraper.session.SessionState.PASSWORD_ACCEPTED;
-
 public class SantanderSession {
     private final RequestHandler requestHandler;
-    private SessionState state;
     private String currentReferer;
 
     private String passwordPath = "";
@@ -24,25 +18,17 @@ public class SantanderSession {
     public SantanderSession(RequestHandler requestHandler) {
         this.requestHandler = requestHandler;
         requestHandler.setSession(this);
-        this.state = NONE;
     }
 
     public void sendNikRequest(String nik) {
-        if (state != NONE) {
-            throw new IllegalStateException("Wrong order of requests.");
-        }
         String pathForXml = requestHandler.sendLoginPageRequest();
         String pathForNikPage = requestHandler.sendRedirectXmlRequest(pathForXml);
         String pathForPassPage = requestHandler.sendNikRequest(pathForNikPage, nik);
 
         passwordPath = pathForPassPage;
-        state = NIK_ACCEPTED;
     }
 
     public void sendPasswordRequest(String password) {
-        if (state != NIK_ACCEPTED) {
-            throw new IllegalStateException("Wrong order of requests.");
-        }
         Map<PathsNames,String> paths = requestHandler.sendPasswordPageRequest(passwordPath);
 
         String pathForPasswordRequest = paths.get(PathsNames.PASSWORD);
@@ -51,7 +37,6 @@ public class SantanderSession {
         requestHandler.sendSessionMapRequest(pathForSessionMap);
         pauseExecution();
         tokenConfirmationPath = requestHandler.sendPasswordRequest(pathForPasswordRequest, password);
-        state = PASSWORD_ACCEPTED;
     }
 
     private void pauseExecution() {
@@ -63,27 +48,17 @@ public class SantanderSession {
     }
 
     public void sendTokenRequest(String token) {
-        if (state != PASSWORD_ACCEPTED || tokenConfirmationPath.isEmpty()) {
-            throw new IllegalStateException("Wrong order of requests.");
-        }
         Map<PathsNames,String> paths = requestHandler.sendTokenRequest(tokenConfirmationPath, token);
 
         logOutPath = paths.get(PathsNames.LOGOUT);
         productsPath = paths.get(PathsNames.PRODUCTS);
-        state = FULLY_LOGGED;
     }
 
     public List<AccountDetails> sendAccountsDetailsRequest() {
-        if (state != FULLY_LOGGED || productsPath.isEmpty()) {
-            throw new IllegalStateException("Wrong order of requests.");
-        }
         return requestHandler.scrapeAccountsInformation(productsPath);
     }
 
     public void logOut() {
-        if (state != FULLY_LOGGED) {
-            throw new IllegalStateException("Wrong order of requests.");
-        }
        requestHandler.sendLogoutRequest(logOutPath);
     }
 
--- src/main/java/scraper/session/SessionState.java
+++
@@ -1,8 +0,0 @@
-package scraper.session;
-
-public enum SessionState {
-    NONE,
-    NIK_ACCEPTED,
-    PASSWORD_ACCEPTED,
-    FULLY_LOGGED;
-}
