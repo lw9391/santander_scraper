e60b72b202ccb03712c207f120d18231cdac5ea3
lw9391 <luk.wychowaniec@gmail.com>
2021-09-11 17:24:24 +0200

    RequestHandler code refactor
    Decoupled some of the common code of public methods into private method.

--- src/main/java/scraper/santander/session/RequestHandler.java
+++ src/main/java/scraper/santander/session/RequestHandler.java
@@ -7,12 +7,13 @@ import scraper.connections.RequestDto;
 import scraper.connections.ResponseDto;
 import scraper.santander.DataScraper;
 import scraper.santander.PathsNames;
-import scraper.santander.SantanderAccountsScraper;
 import scraper.util.DataBuilder;
 
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class RequestHandler {
     private final HttpRequestSender sender;
@@ -38,54 +39,30 @@ public class RequestHandler {
     }
 
     public String sendLoginPageRequest() {
-        RequestDto requestDto = provider.GETLoginPage();
-        ResponseDto response = sender.sendGET(requestDto);
-
-        if (!(response.getStatus() == 200)) {
-            throw new RuntimeException("Status code error during getting login page.");
-        }
-
-        String scrapedPath =  dataScraper.scrapeXmlPathFromLoginPage(response.getResponseBody());
-        session.updateReferer(response.getRequestUrl());
-        return scrapedPath;
+        ProcessedResult<String> processedResult = sendAndProcess(provider::GETLoginPage, sender::sendGET, dataScraper::scrapeXmlPathFromLoginPage);
+        session.updateReferer(processedResult.response.getRequestUrl());
+        return processedResult.data;
     }
 
     public String sendRedirectXmlRequest(String queryParam) {
         long timestamp = new Date().getTime();
         String queryForXml = queryParam + "&_=" + timestamp;
 
-        RequestDto requestDto = provider.GETXmlWithPathForNikPage(queryForXml, session.getCurrentReferer());
-        ResponseDto response = sender.sendGET(requestDto);
-
-        if (!(response.getStatus() == 200)) {
-            throw new RuntimeException("Status code error during getting redirect xml.");
-        }
-
-        return dataScraper.scrapeNikPagePathFromRedirectXml(response.getResponseBody());
+        Supplier<RequestDto> request = () -> provider.GETXmlWithPathForNikPage(queryForXml, session.getCurrentReferer());
+        ProcessedResult<String> processedResult = sendAndProcess(request, sender::sendGET, dataScraper::scrapeNikPagePathFromRedirectXml);
+        return processedResult.data;
     }
 
     public String sendNikRequest(String queryParam, String nik) {
-        RequestDto requestDto = provider.POSTNik(queryParam, nik, session.getCurrentReferer());
-        ResponseDto response = sender.sendPOST(requestDto);
-
-        if (!(response.getStatus() == 200)) {
-            throw new RuntimeException("Status code error during sending nik.");
-        }
-
-        return dataScraper.scrapePasswordPagePathFromNikResponse(response.getResponseBody());
+        Supplier<RequestDto> request = () -> provider.POSTNik(queryParam, nik, session.getCurrentReferer());
+        return sendAndProcess(request, sender::sendPOST, dataScraper::scrapePasswordPagePathFromNikResponse).data;
     }
 
     public Map<PathsNames,String> sendPasswordPageRequest(String path) {
-        RequestDto requestDto = provider.GETPasswordPage(path, session.getCurrentReferer());
-        ResponseDto response = sender.sendGET(requestDto);
-
-        if (!(response.getStatus() == 200)) {
-            throw new RuntimeException("Status code error during getting password page");
-        }
-
-        Map<PathsNames, String> paths = dataScraper.scrapePathsFromPasswordPage(response.getResponseBody());
-        session.updateReferer(response.getRequestUrl());
-        return paths;
+        Supplier<RequestDto> request = () -> provider.GETPasswordPage(path, session.getCurrentReferer());
+        ProcessedResult<Map<PathsNames, String>> processedResult = sendAndProcess(request, sender::sendGET, dataScraper::scrapePathsFromPasswordPage);
+        session.updateReferer(processedResult.response.getRequestUrl());
+        return processedResult.data;
     }
 
     public void sendSessionMapRequest(String path) {
@@ -94,75 +71,79 @@ public class RequestHandler {
         long timestamp = new Date().getTime();
         String queryParams = dataBuilder.buildQueryParams("sessionMap", mapSettings, "_", String.valueOf(timestamp));
 
-        RequestDto requestDto = provider.GETSendSessionMap(path + queryParams, session.getCurrentReferer());
-        ResponseDto response = sender.sendGET(requestDto);
-
-        if (!(response.getStatus() == 200)) {
-            throw new RuntimeException("Status code error during sending session map.");
-        }
+        Supplier<RequestDto> request = () -> provider.GETSendSessionMap(path + queryParams, session.getCurrentReferer());
+        sendAndProcess(request, sender::sendGET, Function.identity());
     }
 
     public String sendPasswordRequest(String path, String password) {
-        RequestDto requestDto = provider.POSTPassword(path, password, session.getCurrentReferer());
-        ResponseDto response = sender.sendPOST(requestDto);
-
-        if (!(response.getStatus() == 200)) {
-            throw new RuntimeException("Status code error during sending password");
-        }
-        String tokenPath = dataScraper.scrapeTokenPathFromPasswordResponse(response.getResponseBody());
-
-        session.updateReferer(response.getRequestUrl());
-        return tokenPath;
+        Supplier<RequestDto> request = () -> provider.POSTPassword(path, password, session.getCurrentReferer());
+        ProcessedResult<String> processedResult = sendAndProcess(request, sender::sendPOST, dataScraper::scrapeTokenPathFromPasswordResponse);
+        session.updateReferer(processedResult.response.getRequestUrl());
+        return processedResult.data;
     }
 
     public Map<PathsNames,String> sendTokenRequest(String tokenConfirmationPath, String token) {
-        RequestDto requestDto = provider.POSTToken(tokenConfirmationPath, token, session.getCurrentReferer());
-        ResponseDto response = sender.sendPOST(requestDto);
-
-        if (!(response.getStatus() == 200)) {
-            throw new RuntimeException("Status code error during sending token");
-        }
-        if (!validateLoginCorrectness(response)) {
+        Supplier<RequestDto> request = () -> provider.POSTToken(tokenConfirmationPath, token, session.getCurrentReferer());
+        ProcessedResult<String> processedResult = sendAndProcess(request, sender::sendPOST, dataScraper::scrapeInvalidLoginDiv);
+        if (!processedResult.data.isEmpty()) {
             throw new InvalidCredentialsException("Login failed, provided incorrect password or token.");
         }
-        Map<PathsNames,String> paths = dataScraper.scrapePathsFromDashboardPage(response.getResponseBody());
+        Map<PathsNames,String> paths = dataScraper.scrapePathsFromDashboardPage(processedResult.response.getResponseBody());
 
-        session.updateReferer(response.getRequestUrl());
+        session.updateReferer(processedResult.response.getRequestUrl());
         return paths;
     }
 
-    private boolean validateLoginCorrectness(ResponseDto tokenPOSTResponse) {
-        String logoutDiv = dataScraper.scrapeInvalidLoginDiv(tokenPOSTResponse.getResponseBody());
-        return logoutDiv.isEmpty();
-    }
-
     public List<AccountDetails> scrapeAccountsInformation(String path) {
-        RequestDto requestDto = provider.GETProductsPage(path, session.getCurrentReferer());
-        ResponseDto response = sender.sendGET(requestDto);
+        Supplier<RequestDto> request = () -> provider.GETProductsPage(path, session.getCurrentReferer());
+        Supplier<RequestDto> logout = () -> provider.GETEmergencyLogout(session.getCurrentReferer());
+        ProcessedResult<List<AccountDetails>> processedResult = sendAndProcess(request, sender::sendGET, dataScraper::scrapeAccountsInformationFromProductsPage, logout);
 
-        if (!(response.getStatus() == 200)) {
-            RequestDto logout = provider.GETEmergencyLogout(session.getCurrentReferer());
-            sender.sendGET(logout);
-            throw new RuntimeException("Status code error during getting product page.");
-        }
-
-        return dataScraper.scrapeAccountsInformationFromProductsPage(response.getResponseBody());
+        return processedResult.data;
     }
 
     public void sendLogoutRequest(String query) {
-        RequestDto requestDto = provider.GETLogout(query, session.getCurrentReferer());
-        ResponseDto response = sender.sendGET(requestDto);
-        if (!(response.getStatus() == 200)) {
-            throw new RuntimeException("Status code error during logout");
-        }
+        Supplier<RequestDto> request = () -> provider.GETLogout(query, session.getCurrentReferer());
+        ProcessedResult<String> processedResult = sendAndProcess(request, sender::sendGET, Function.identity());
 
-        if (!response.getRequestUrl().equals(provider.LOGOUT)) {
+        if (!processedResult.response.getRequestUrl().equals(provider.LOGOUT)) {
             RequestDto logout = provider.GETEmergencyLogout(session.getCurrentReferer());
             sender.sendGET(logout);
         }
+    }
+
+    private static <T> ProcessedResult<T> sendAndProcess(Supplier<RequestDto> requestSupplier, Function<RequestDto, ResponseDto> httpSendMethod, Function<String,T> scrapingFunction) {
+        RequestDto requestDto = requestSupplier.get();
+        ResponseDto responseDto = httpSendMethod.apply(requestDto);
+        if (!(responseDto.getStatus() == 200)) {
+            throw new RuntimeException("Status code error during getting login page.");
+        }
+        T result = scrapingFunction.apply(responseDto.getResponseBody());
+        return new ProcessedResult<T>(responseDto, result);
+    }
+
+    private static <T> ProcessedResult<T> sendAndProcess(Supplier<RequestDto> requestSupplier, Function<RequestDto, ResponseDto> httpSendMethod, Function<String,T> scrapingFunction, Supplier<RequestDto> logout) {
+        RequestDto requestDto = requestSupplier.get();
+        ResponseDto responseDto = httpSendMethod.apply(requestDto);
+        if (!(responseDto.getStatus() == 200)) {
+            httpSendMethod.apply(logout.get());
+            throw new RuntimeException("Status code error during getting login page.");
+        }
+        T result = scrapingFunction.apply(responseDto.getResponseBody());
+        return new ProcessedResult<T>(responseDto, result);
     }
 
     public void setSession(SantanderSession session) {
         this.session = session;
+    }
+
+    private static class ProcessedResult<T> {
+        public final ResponseDto response;
+        public final T data;
+
+        public ProcessedResult(ResponseDto response, T data) {
+            this.response = response;
+            this.data = data;
+        }
     }
 }
