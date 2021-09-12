8bc258ca1b342affb8eafb802bde79d1bec312f0
lw9391 <luk.wychowaniec@gmail.com>
2021-09-12 19:54:56 +0200

    Remove unnecessery requests headers
    Removed headers that are automatically handled by OkHttpClient.

--- src/main/java/scraper/santander/session/SantanderRequestProvider.java
+++ src/main/java/scraper/santander/session/SantanderRequestProvider.java
@@ -4,9 +4,6 @@ import scraper.connections.RequestDto;
 
 import static scraper.Header.ACCEPT;
 import static scraper.Header.ACCEPT_LANGUAGE;
-import static scraper.Header.CACHE_CONTROL;
-import static scraper.Header.CONNECTION;
-import static scraper.Header.CONTENT_TYPE_URLENCODED;
 import static scraper.Header.USER_AGENT;
 
 public class SantanderRequestProvider {
@@ -45,7 +42,6 @@ public class SantanderRequestProvider {
                 .addFormBodyPair("nik", nik)
                 .addFormBodyPair("dp", "")
                 .addFormBodyPair("loginButton", "1")
-                .setHeader(CONTENT_TYPE_URLENCODED.name, CONTENT_TYPE_URLENCODED.value)
                 .setHeader("Referer", referer)
                 .setHeader("Wicket-Ajax", "true")
                 .setHeader("Wicket-Ajax-BaseURL", referer.substring(referer.indexOf("login?")))
@@ -80,7 +76,6 @@ public class SantanderRequestProvider {
         return builder.setUrl(HOST + PATH + path)
                 .addFormBodyPair("pinFragment:pin", password)
                 .addFormBodyPair("loginButton", "Dalej")
-                .setHeader(CONTENT_TYPE_URLENCODED.name, "application/x-www-form-urlencoded")
                 .setHeader("Referer", referer)
                 .build();
     }
@@ -91,7 +86,6 @@ public class SantanderRequestProvider {
         return builder.setUrl(HOST + PATH + path)
                 .addFormBodyPair("response", token)
                 .addFormBodyPair("loginButton", "Dalej")
-                .setHeader(CONTENT_TYPE_URLENCODED.name, "application/x-www-form-urlencoded")
                 .setHeader("Referer", referer)
                 .build();
     }
@@ -123,9 +117,6 @@ public class SantanderRequestProvider {
     private static RequestDto.Builder setCommonHeaders(RequestDto.Builder builder) {
         return builder.setHeader(ACCEPT.name, ACCEPT.value)
                 .setHeader(ACCEPT_LANGUAGE.name, ACCEPT_LANGUAGE.value)
-                .setHeader(CACHE_CONTROL.name, CACHE_CONTROL.value)
-                .setHeader(CONNECTION.name, CONNECTION.value)
                 .setHeader(USER_AGENT.name, USER_AGENT.value);
     }
-
 }
