7c1c8a781f9ea4d58ea4953099860ba2769f9239
lw9391 <luk.wychowaniec@gmail.com>
2021-10-13 14:10:42 +0200

    Refactor AcceptanceTest
    Minor changes for better readability.

--- src/test/java/AcceptanceTest.java
+++ src/test/java/AcceptanceTest.java
@@ -1,39 +1,39 @@
 import scraper.App;
-import scraper.santander.Credentials;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Scanner;
 
 import static scraper.santander.SantanderAccountsScraper.PROMPT_FOR_SMS_CODE;
 
 public class AcceptanceTest {
-  private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-  private static final PrintStream originalOut = System.out;
-  private static final Credentials credentials = readCredentials();
 
-  public static void main(String[] args) throws IOException {
-    System.setOut(new PrintStream(outContent));
-    App.main(credentials.accountNumber, credentials.password);
-    String scrapedAccountsData = outContent.toString();
-    System.setOut(originalOut);
+  public static void main(String[] args) {
+    PrintStream outContent = new PrintStream(new ByteArrayOutputStream());
+    PrintStream originalSystemOut = System.out;
+    String[] credentials = readCredentials();
+
+    System.setOut(outContent);
+    App.main(credentials[0], credentials[1]);
+    String gatheredOutput = outContent.toString();
+    System.setOut(originalSystemOut);
     outContent.close();
-    if (scrapedAccountsData.length() > PROMPT_FOR_SMS_CODE.length()) {
+
+    if (gatheredOutput.length() > PROMPT_FOR_SMS_CODE.length()) {
       System.out.println("Test passed");
     } else {
       System.out.println("Test failed");
     }
   }
 
-  private static Credentials readCredentials() {
+  private static String[] readCredentials() {
     Scanner credentialsFileScanner = initFileScanner();
     String nik = credentialsFileScanner.nextLine();
     String password = credentialsFileScanner.nextLine();
     credentialsFileScanner.close();
-    return new Credentials(nik, password);
+    return new String[]{nik, password};
   }
 
   private static Scanner initFileScanner() {
